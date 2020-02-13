b72b41272198626f0d302682f48e702d249e14f8
ilydcndr <ilaydacundar@hotmail.com>
2020-02-13 20:58:56 +0300

    hours

--- weather/src/App.js
+++ weather/src/App.js
@@ -49,18 +49,27 @@ class App extends Component {
     const year=d.getFullYear()
     const day=d.getDay()
     const array=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    
+    const getDay=array[day]
+   return(
+    `${getDay},  ${dayNumber}.${month}.${year} |`
+   )
+  }
+
+  getHours=()=>{
+    const d = new Date()
     const s=d.getHours();
     const m= d.getMinutes()
     const sn=d.getSeconds()
-    const getDay=array[day]
-   return(
-    `${getDay},  ${dayNumber}.${month}.${year} | ${s}:${m}:${sn}`
-   )
+    return(
+      `${s}:${m}:${sn}`
+    )
   }
   
   render() {
     return (
       <div>
+        <h5 className="text-right mt-3 mr-5">{this.getHours()}</h5>
         <InputComp saveValue={this.saveValue} search={this.search} valueCity={this.state.valueCity} valueCountry={this.state.valueCountry}/>
         <WeatherComp getTime={this.getTime} location={this.state.location}/>
       </div>
--- weather/src/Components/InputComp.js
+++ weather/src/Components/InputComp.js
@@ -5,12 +5,13 @@ import './InputComp.css'
  function InputComp(props) {
         return (
             <div>
+                <div></div>
                 <Form className="row justify-content-center ">
                    <Input className="col-4 mt-5 mr-2" type="text" name="city" value={props.valueCity} id="city" placeholder="City" onChange={(e)=>{props.saveValue(e,'city')}} />
                    <Input className="col-4 mt-5 ml-2" type="text" name="country" value={props.valueCountry}  id="country" placeholder="Country" onChange={(e)=>{props.saveValue(e,'country')}} />
                 </Form>
                 <div className="row justify-content-center mt-3 ">
-                <Button outline color="primary" onClick={()=>{props.search()}}>Search</Button>
+                <Button  color="primary" onClick={()=>{props.search()}}>Search</Button>
                 </div> 
             </div>
         )
